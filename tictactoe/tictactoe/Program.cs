7de7b0de85e02edba4750917a53e83494a23bf74
Maya Montague <mayamontague4@gmail.com>
2024-06-19 19:26:30 -0500

    .

--- tictactoe/tictactoe/Program.cs
+++ tictactoe/tictactoe/Program.cs
@@ -1,22 +1,163 @@
-﻿﻿namespace tictactoe;
-
-class Program
+﻿﻿namespace tictactoe
 {
-    static void Main()
+    class Program
     {
-        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+        public class Game
+        {
+            private GameBoard gameBoard;
+            private Player player1;
+            private Player player2;
+            private Player currentPlayer;
 
-        Console.Write("Enter name for Player 1: ");
-        string player1Name = Console.ReadLine();
-        Player player1 = new Player(player1Name, "X");
+            public Game(Player player1, Player player2)
+            {
+                gameBoard = new GameBoard();
+                this.player1 = player1;
+                this.player2 = player2;
+                currentPlayer = player1;
+            }
 
-        Console.Write("Enter name for Player 2: ");
-        string player2Name = Console.ReadLine();
-        Player player2 = new Player(player2Name, "O");
+            public void Play()
+            {
+                while (true)
+                {
+                    Console.Clear();
+                    gameBoard.DisplayBoard();
+                    Console.WriteLine($"{currentPlayer.Name}'s ({currentPlayer.Marker}) turn. Choose your position: ");
+                    int position;
+                    while (!int.TryParse(Console.ReadLine(), out position) || position < 1 || position > 9 || !gameBoard.UpdateBoard(position, currentPlayer.Marker))
+                    {
+                        Console.WriteLine("Invalid input, please choose an empty position between 1 and 9: ");
+                    }
 
-        Game game = new Game(player1, player2);
-        game.Play();
+                    if (gameBoard.CheckForWinner())
+                    {
+                        Console.Clear();
+                        gameBoard.DisplayBoard();
+                        Console.WriteLine($"{currentPlayer.Name} wins!");
+                        break;
+                    }
 
-        Console.WriteLine("Thanks for playing!");
+                    if (gameBoard.IsFull())
+                    {
+                        Console.Clear();
+                        gameBoard.DisplayBoard();
+                        Console.WriteLine("It's a draw!");
+                        break;
+                    }
+
+                    currentPlayer = currentPlayer == player1 ? player2 : player1;
+                }
+
+                Console.WriteLine("Do you want to play again? (y/n)");
+                string response = Console.ReadLine();
+                if (response?.ToLower() == "y")
+                {
+                    gameBoard.Reset();
+                    currentPlayer = player1;
+                    Play();
+                }
+            }
+        }
+
+        public class GameBoard
+        {
+            private string[] board;
+
+            public GameBoard()
+            {
+                board = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            }
+
+            public void DisplayBoard()
+            {
+                Console.WriteLine(" {0} | {1} | {2} ", board[0], board[1], board[2]);
+                Console.WriteLine("---|---|---");
+                Console.WriteLine(" {3} | {4} | {5} ", board[3], board[4], board[5]);
+                Console.WriteLine("---|---|---");
+                Console.WriteLine(" {6} | {7} | {8} ", board[6], board[7], board[8]);
+            }
+
+            public bool UpdateBoard(int position, string marker)
+            {
+                if (position >= 1 && position <= 9 && board[position - 1] != "X" && board[position - 1] != "O")
+                {
+                    board[position - 1] = marker;
+                    return true;
+                }
+                return false;
+            }
+
+            public bool CheckForWinner()
+            {
+                string[][] winningCombinations = new string[][]
+                {
+                    new string[] { board[0], board[1], board[2] },
+                    new string[] { board[3], board[4], board[5] },
+                    new string[] { board[6], board[7], board[8] },
+                    new string[] { board[0], board[3], board[6] },
+                    new string[] { board[1], board[4], board[7] },
+                    new string[] { board[2], board[5], board[8] },
+                    new string[] { board[0], board[4], board[8] },
+                    new string[] { board[2], board[4], board[6] }
+                };
+
+                foreach (var combo in winningCombinations)
+                {
+                    if (combo[0] == combo[1] && combo[1] == combo[2])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public bool IsFull()
+            {
+                foreach (var spot in board)
+                {
+                    if (spot != "X" && spot != "O")
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            public void Reset()
+            {
+                board = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            }
+        }
+
+        public class Player
+        {
+            public string Name { get; set; }
+            public string Marker { get; set; }
+
+            public Player(string name, string marker)
+            {
+                Name = name;
+                Marker = marker;
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+            Console.Write("Enter name for Player 1: ");
+            string player1Name = Console.ReadLine();
+            Player player1 = new Player(player1Name, "X");
+
+            Console.Write("Enter name for Player 2: ");
+            string player2Name = Console.ReadLine();
+            Player player2 = new Player(player2Name, "O");
+
+            Game game = new Game(player1, player2);
+            game.Play();
+
+            Console.WriteLine("Thanks for playing!");
+        }
     }
 }
--- tictactoe/tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21e18e684b47a73b96bc284da876a126f8f6eb88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683528d9a99e402e1d2583e8d283603843706641")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
